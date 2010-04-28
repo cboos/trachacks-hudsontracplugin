d8e8ae60a493d0aac0eefb76fa3228ce48746596
Christian Boos <cboos@neuf.fr>
2010-04-28 11:30:19 +0200

    Updated contact information for the 0.11 and 0.12 branches.

--- setup.py
+++ setup.py
@@ -7,10 +7,14 @@ setup(
     name = 'HudsonTrac',
     version = '0.11.0.3',
     author = "Ronald Tschalär",
+    maintainer = "Christian Boos",
     description = "A trac plugin to add hudson build info to the trac timeline",
     license = "BSD",
     keywords = "trac builds hudson",
-    url = "http://trac-hacks.org/wiki/HudsonTracPlugin",
+    # use this to report bugs to 0.11 and 0.12 branches of this plugin:
+    url = "http://github.com/cboos/trachacks-hudsontracplugin",
+    # original was:
+    #url = "http://trac-hacks.org/wiki/HudsonTracPlugin",
 
     packages = ['HudsonTrac'],
     package_data = {
